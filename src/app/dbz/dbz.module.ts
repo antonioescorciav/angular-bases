f63d4fc53ba9419d700414203f95609223ed220a
antonioescorciav <antonio.escorciav@gmail.com>
2023-05-19 10:16:06 -0500

    segunda parte - ejercicio DBZ

--- src/app/dbz/dbz.module.ts
+++ src/app/dbz/dbz.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MainPageComponent } from './pages/main-pages.component';
+import { ListComponent } from './component/list/list.component';
+import { AddCharacterComponent } from './component/add-character/add-character.component';
 
 
 
 @NgModule({
   declarations: [
-    MainPageComponent
+    MainPageComponent,
+    ListComponent,
+    AddCharacterComponent
   ],
   imports: [
     CommonModule,
--- src/app/dbz/pages/main-pages.component.ts
+++ src/app/dbz/pages/main-pages.component.ts
@@ -1,13 +1,26 @@
 
 import { Component} from '@angular/core';
+import { Character } from '../interfaces/character.interfaces';
 
 @Component({
   selector: 'app-dbz-main-page',
   templateUrl: './main.component.html'
 })
 
+
 export class MainPageComponent {
 
+  public characters: Character[] = [{
+    name: 'Krillin',
+    power: 1000
+  },{
+    name: 'Goku',
+    power: 9500
+  }, {
+    name: 'Vegetta',
+    power: 9000
+  }
+];
 
 
 }
